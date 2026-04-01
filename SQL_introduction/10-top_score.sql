86b17e7365507f173822fe510af4df15e25a3de3
tiyegordon <tiye.gordon@gmail.com>
2026-03-31 21:27:17 -0400

    all tasks

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Lists all records of the table by description
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- computes the score averagte of all records in the table
+SELECT AVG(score) AS average
+FROM second_table
